3a55b5e1bac0f894ad6242844e22ac5db7198e7c
Jesus GM <jgomata@gmail.com>
2023-04-01 09:45:30 -0600

    new file:   gatsby-config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   src/images/icon.png 	new file:   src/pages/404.js 	new file:   src/pages/index.js

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Mi primer sitio Gatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
